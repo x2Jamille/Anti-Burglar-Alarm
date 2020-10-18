6e13aba4cf37d34c54e4cc673a97dacacc9b1ca5
x2Jamille <x2Jamille@users.noreply.github.com>
2020-10-18 22:03:20 +0300

    Init

---
+++ Assets/Scripts/Alarm.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Alarm : MonoBehaviour
+{
+
+    private AudioSource _alarm;
+    private bool _isBurglarInside;
+
+    [SerializeField] private float _initialVolume;
+    [SerializeField] private float _volumeUpStep;
+
+    private void Awake()
+    {
+        _alarm = GetComponent<AudioSource>();
+        _alarm.volume = _initialVolume;
+    }
+
+    private void Update()
+    {
+        if (_isBurglarInside)
+        {
+            _alarm.volume += _volumeUpStep * Time.deltaTime;
+        }
+        else
+        {
+            if (_alarm.volume <= _initialVolume)
+                _alarm.Stop();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<BurglarMovement>())
+        {
+            _isBurglarInside = true;
+            _alarm.Play();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<BurglarMovement>())
+        {
+            _isBurglarInside = false;
+            _alarm.DOFade(_initialVolume, 3);
+        }
+    }
+}
---
+++ Assets/Scripts/BurglarMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurglarMovement : MonoBehaviour
+{
+    [SerializeField]private bool _isLookingToRight;
+    [SerializeField] private float _speed = 3;
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+        if (xAxis < 0 && _isLookingToRight)
+            FlipSpriteX();
+        else if (xAxis > 0 && !_isLookingToRight)
+            FlipSpriteX();
+        transform.position += new Vector3(xAxis, yAxis) * _speed * Time.deltaTime;
+    }
+
+    private void FlipSpriteX()
+    {
+        _isLookingToRight = !_isLookingToRight;
+        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+    }
+}
